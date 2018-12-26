15f021b69618b59c342954fc7642bac2f97ad62e
kshinji911 <kshinji911@gmail.com>
2018-12-26 14:38:20 -0500

    initial

---
+++ assets/javascript/app.js
@@ -0,0 +1,18 @@
+//ready js
+$(document).ready(function () {
+   let topics = ["rick and morty", "family guy", "adventure time", "steven universe", "spongebob squarepants", "venture bros", "simpsons"]
+
+   //initialize variables here
+
+   //store divs in variables
+   searchbtn = $("#searchbtn");
+   searchInput = $("#searchInput");
+   addBtn = $("#addBtn");
+   
+   //function to make button and add on to searchbtn div
+   function makeBtn() {
+
+   }
+
+   //end ready js
+});
